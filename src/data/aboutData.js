70a578490b5de5588340066174fec2d5d54fe576
raviraj3214 <raviraj32140@gmail.com>
2023-11-17 21:01:16 +0530

    pipeline set completely

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
     title: "Who I am",
-    description1: "My name is Ravi Raj and my branch is C.S.E and I graduated from M.D.U recently.I am well trained Full Stack Developer certified from Coding Ninjas and also have office work experience as a React Developer Intern in Dtroffle Pvt. Info. where I developed frontend of the project in react.js.I am also good in coding and I got certification in C++ and DSA.I also a certified coder on HackerRank where I coded in C++ and Java.",
+    description1: "My name is Ravi Raj and I graduated from M.D.U recently in 2023 and my branch is C.S.E.I am well trained Full Stack Developer certified from Coding Ninjas and also have office work experience as a React Developer Intern in Dtroffle Pvt. Info. where I developed frontend of the project in react.js.I am also good in coding and I got certification in C++ and DSA.I also a certified coder on HackerRank where I coded in C++ and Java.",
     image: 2
 }
